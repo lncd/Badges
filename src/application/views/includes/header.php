78e037fd538097078bc917d3a0ad2e48f0bbb1da
Jamie Mahoney <jmahoney127@googlemail.com>
2013-01-17 16:10:03 +0000

    Altered header view

--- src/application/views/includes/header.php
+++ src/application/views/includes/header.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title><?php if(isset($title)) echo $title . ' - '; ?>Assignment Wizard	</title>
+	<title>Badges</title>
 	<meta name="description" content="The Common Web Design is the new branding for the University of Lincoln's online services">
 	<meta name="author" content="Online Services Team; ost@lincoln.ac.uk">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,12 +25,12 @@
 	<script src="<?php echo $_SERVER['CWD_BASE_URI']; ?>/plugins.min.js"></script>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 	<script type="text/javascript" src="https://github.com/loopj/jquery-tokeninput/raw/master/src/jquery.tokeninput.js"></script>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/token-input-bootstrap.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>custom.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>scripts/markitup/skins/simple/style.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>scripts/markitup/sets/default/style.css" />
-	<script type="text/javascript" src="<?php echo base_url(); ?>scripts/markitup/jquery.markitup.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>scripts/markitup/sets/default/set.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>assets/css/token-input-bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>custom.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>scripts/markitup/skins/simple/style.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>scripts/markitup/sets/default/style.css" />
+	<script type="text/javascript" src="<?php echo site_url(); ?>scripts/markitup/jquery.markitup.js"></script>
+	<script type="text/javascript" src="<?php echo site_url(); ?>scripts/markitup/sets/default/set.js"></script>
 </head>
 
 <body>
@@ -49,19 +49,13 @@
 
 					<hgroup id="cwd-hgroup">
 
-						<h1>Assignment Wizard<small>Alpha</small></h1>
+						<h1>Badges<small>Alpha</small></h1>
 					</hgroup>
 
 					<div class="navbar">
 						<div class="navbar-inner">
 						<ul class="nav">
 							<li><a href="<?php echo site_url(); ?>">Home</a></li>
-							<?php if ($this->session->userdata('user_id')) :?>
-							<li><a href="<?php echo $_SERVER['BASE_URL']; ?>signin/logout">Logout</a></li>
-							<?php else: ?>
-							<!--<li><a href="https://sso.lincoln.ac.uk/oauth?response_type=code&client_id=<?php echo $_SERVER['OAUTH_ID']; ?>&redirect_uri=<?php echo $_SERVER['OAUTH_REDIRECT'] ;?>&scope=user.basic,user.contact&state=test">Login</a></li>-->
-							<li><a href="landing_page/fake_login">Login</a></li>
-							<?php endif;?>
 						</ul>
 						</div>
 					</div>

3a64734dd37ad18b40b781388048baa24918ab11
Jamie Mahoney <jmahoney127@googlemail.com>
2013-01-18 10:38:12 +0000

    Added awarded badges to profile view.

--- src/application/views/home.php
+++ src/application/views/home.php
@@ -44,6 +44,24 @@
 
 		<section class="span12" id="badges_waiting">
 			<h2>Completed and Collected Badges</h2>
+			<?php if((isset($awarded)) AND (sizeof($awarded) > 0)): ?>
+			<table class="table table-striped table-bordered table-condensed">
+			<?php foreach($awarded as $a_badge): ?>
+			<tr style="width: 580px !important">
+				<td style="width: 80px; text-align: center !important"><img src="<?php echo site_url() . 'assets/uploads/' . $a_badge->badge->badge_image;?>" style="width: 60px; height: 60px; margin: 10px"></td>
+				<td style="width: 500px"><span class="badge_name"><h4><?php echo $a_badge->badge->badge_name;?></h4></span>
+					<p><?php echo $a_badge->badge->description;?></p>
+					<p>Criteria:</p>
+					<ul>
+						<?php foreach($a_badge->objectives as $objective): ?>
+							<li><?php echo $objective->objective_text; ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</td>
+				</tr>
+			<?php endforeach;?>
+			</table>
+			<?php endif; ?>
 		</section>
 	</div>
 </div>

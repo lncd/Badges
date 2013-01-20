8ffce1b7b901055c17421e5bfaa669496414d6c4
Jamie Mahoney <jmahoney127@googlemail.com>
2013-01-20 13:31:26 +0000

    Tab indentation and other minor style fixes.

--- src/application/models/badge_model.php
+++ src/application/models/badge_model.php
@@ -23,66 +23,66 @@
 */
 class Badge_model extends CI_Model
 {
-    /**
-    * Constructor.
-    *
-    * @access Public
-    * @return Nothing
-    */
-    function __construct()
-    {
-        // Call the Model constructor
-        parent::__construct();
-    }
-    
-    /**
-    * Get badge by id
-    *
-    * @param int $id ID of the badge.
-    *
-    * @access Public
-    * @return Object rep badge.
-    */
-    function get_by_id($id)
-    {
-    	$badge = new Badge();
-    	$badge->where('id', $id)->get();
+	/**
+	* Constructor.
+	*
+	* @access Public
+	* @return Nothing
+	*/
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+	}
+	
+	/**
+	* Get badge by id
+	*
+	* @param int $id ID of the badge.
+	*
+	* @access Public
+	* @return Object rep badge.
+	*/
+	function get_by_id($id)
+	{
+		$badge = new Badge();
+		$badge->where('id', $id)->get();
 
-        $temp = new stdClass();
-        $temp->badge = $badge->stored;
+		$temp = new stdClass();
+		$temp->badge = $badge->stored;
 
-        //Get objectives for this badge
-        $badge_objectives = new Badge_objective();
-        $badge_objectives->where('badge_id', $id)->get();
+		//Get objectives for this badge
+		$badge_objectives = new Badge_objective();
+		$badge_objectives->where('badge_id', $id)->get();
 
-        foreach($badge_objectives as $badge_obj)
-        {
-            $objective = new Objective();
-            $objective->where('id', $badge_obj->objective_id)->get();
-            $temp->objectives[] = $objective->stored;
-            unset($objective);
-        }
-    	return $temp;
-    }
+		foreach($badge_objectives as $badge_obj)
+		{
+			$objective = new Objective();
+			$objective->where('id', $badge_obj->objective_id)->get();
+			$temp->objectives[] = $objective->stored;
+			unset($objective);
+		}
+		return $temp;
+	}
 
-    /**
-    * Returns a speciic badge instance
-    *
-    * @param int $user_id ID of the user.
-    * @param int $badge_id ID of the badge.
-    *
-    * @access Public
-    * @return Nothing
-    */
-    function get_instance($user_id, $badge_id)
-    {
-        $badge_earned = new Badge_earned();
-        $badge_earned->where('user_id', $user_id);
-        $badge_earned->where('badge_id', $badge_id);
-        $badge_earned->get();
+	/**
+	* Returns a speciic badge instance
+	*
+	* @param int $user_id  ID of the user.
+	* @param int $badge_id ID of the badge.
+	*
+	* @access Public
+	* @return Nothing
+	*/
+	function get_instance($user_id, $badge_id)
+	{
+		$badge_earned = new Badge_earned();
+		$badge_earned->where('user_id', $user_id);
+		$badge_earned->where('badge_id', $badge_id);
+		$badge_earned->get();
 
-        return $badge_earned->stored;
-    }
+		return $badge_earned->stored;
+	}
 }
 
 // End of file badge_model.php

d9111899600c05e743999415ed40ed9365ed9e6c
Jamie Mahoney <jmahoney127@googlemail.com>
2013-01-21 09:41:22 +0000

    Basic view and controller for claim.

---
+++ src/application/controllers/claim.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* Claim
+*
+* PHP Version 5
+* 
+* @category  Badges
+* @package   Badges
+* @author    Jamie Mahoney <jmahoney@lincoln.ac.uk>
+* @copyright 2013 University of Lincoln
+* @license   GNU Affero General Public License 3.0
+* @link      coursedata.blogs.lincoln.ac.uk
+*/
+
+/**
+* Claim
+*
+* @category Badges
+* @package  Badges
+* @author   Jamie Mahoney <jmahoney@lincoln.ac.uk>
+* @license  GNU Affero General Public License 3.0
+* @link     coursedata.blogs.lincoln.ac.uk
+*/
+class Claim extends CI_Controller {
+
+	/**
+	* Index function
+	*
+	* @return Nothing
+	* @access Public
+	*/
+	public function index($earned_id)
+	{
+		echo $earned_id;
+	}
+}
+
+// End of file claim.php
+// Location: ./controllers/claim.php
---
+++ src/application/views/claim.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="page-header">
+		<section>
+			<h1>
+				Claim Badge
+				<small></small>
+			</h1>
+		</section>
+	</div>
+</div>
